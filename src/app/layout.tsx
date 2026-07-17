66701a35c15afda055a00b5d3b119c99956ac114
wilzzzzzzzz <itswgxx@gmail.com>
2026-07-17 21:46:59 +0700

    feat: add responsive text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${dancingScript.variable} ${plusJakarta} h-full antialiased`}
+      className={`${dancingScript.variable} ${plusJakarta.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -4,9 +4,9 @@ export default function Hero() {
   return (
     <section className="relative flex items-center justify-center w-full h-screen">
       <div className="flex items-center justify-center w-full h-full bg-black/70 absolute top-0 left-0">
-        <div className="flex flex-col gap-4 text-center text-2xl">
+        <div className="flex flex-col gap-4 text-center text-lg md:text-2xl">
           <span className="font-plusJakarta">Welcome to</span>
-          <h1 className="font-dancingScript text-8xl">
+          <h1 className="font-dancingScript text-4xl md:text-6xl lg:text-8xl">
             Desa <span className="text-orange-300">Tumpakkepuh</span>
           </h1>
         </div>
